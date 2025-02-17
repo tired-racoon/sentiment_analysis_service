b3cea42df7e9635aa1528c5dd4d2e59c2d08770b
Михаил Стрежнев <109974255+tired-racoon@users.noreply.github.com>
2025-02-17 23:36:24 +0300

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 class SentimentAnalyzer:
     def __init__(self):
-        self.model = pipeline("sentiment-analysis", model="sismetanin/xlm_roberta_large-ru-sentiment-sentirueval2016", from_pt=True)
+        self.model = pipeline("sentiment-analysis", model="sismetanin/xlm_roberta_large-ru-sentiment-sentirueval2016")
 
     def predict(self, text):
         result = self.model(text)[0]
