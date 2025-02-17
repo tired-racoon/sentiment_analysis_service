b4cbc1d1207cc0befac457d24a59c27b2a32e960
Михаил Стрежнев <109974255+tired-racoon@users.noreply.github.com>
2025-02-18 00:17:51 +0300

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -5,9 +5,6 @@ import plotly.express as px
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
-import os
-
-os.system('uvicorn backend.main:app --reload --host 0.0.0.0 --port 8001')
 
 
 API_TEXT = "http://127.0.0.1:8000/analyze/"
