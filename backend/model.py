40a79de7f1b603f9e8d2b6a5329bd4c506f31196
Михаил Стрежнев <109974255+tired-racoon@users.noreply.github.com>
2025-02-24 20:09:55 +0300

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 class SentimentAnalyzer:
     def __init__(self):
-        self.model = pipeline(model="tired-racoon/tonika_sentim", trust_remote_code=True)
+        self.model = pipeline(model="sismetanin/xlm_roberta_large-ru-sentiment-sentirueval2016", trust_remote_code=True)
 
     def predict(self, text):
         result = self.model(text)[0]
